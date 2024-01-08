b5e59a695295ea6b9d4e46383b18e13d202d4890
pdrmenezes <pdrmenezes1@gmail.com>
2024-01-08 17:49:48 -0300

    feat:add custom button variant

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button size="lg" variant="ghost">
+    <Button size="lg" variant="custom_example">
       Hello NextAuth Toolkit!
     </Button>
   );
