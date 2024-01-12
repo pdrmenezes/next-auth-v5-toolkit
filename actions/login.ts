e9449ece6ef7b7aa3b87708f2ca973b23aeda801
pdrmenezes <pdrmenezes1@gmail.com>
2024-01-12 20:39:54 -0300

    feat: update login server action to validate credentials and handle errors

--- actions/login.ts
+++ actions/login.ts
@@ -2,11 +2,31 @@
 
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
 export async function login(formData: z.infer<typeof LoginSchema>) {
   const validatedFields = LoginSchema.safeParse(formData);
 
   if (!validatedFields.success) return { error: "Invalid fields" };
 
-  return { success: "Logging in" };
+  const { email, password } = validatedFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+    throw error;
+  }
 }
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,3 +1,21 @@
+import { auth, signOut } from "@/auth";
+
 export default async function SettingsPage() {
-  return <div>Settings Page</div>;
+  const session = await auth();
+  return (
+    <div>
+      <h1>Settings Page</h1>
+      <p>session info: {JSON.stringify(session)}</p>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button className="p-2 outline" type="submit">
+          Sign Out
+        </button>
+      </form>
+    </div>
+  );
 }
