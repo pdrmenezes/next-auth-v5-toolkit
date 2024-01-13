85928946d1ae60b7c1f7eaa37e7a75c401d21765
pdrmenezes <pdrmenezes1@gmail.com>
2024-01-13 13:06:00 -0300

    feat: customizing session to pass in the userId inside the session.user.id new custom field

--- auth.ts
+++ auth.ts
@@ -17,15 +17,14 @@ export const {
     // so the session token is identical to the token returned by the jwt callback
     // and we can customize the session object as well as the token inside the jwt callback to return whatever we want, for instance, add the isNewUser property (e.g.: token.isNewUser = true)
     async session({ session, user, token }) {
-      if (session.user) {
-        session.user.customField = token.customField;
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
       }
       return session;
     },
 
     // user and profile are not as reliable as the token, perhaps they allow for customization but in normal circunstances they may come out as 'undefined', so we'll use the token.sub and pass it as the userId to the session callback to get additional user information
     async jwt({ token }) {
-      token.customField = "test";
       return token;
     },
   },
