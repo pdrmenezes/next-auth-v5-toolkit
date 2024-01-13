d7d77c3214a43d0805150779a45bd63a5881b837
pdrmenezes <pdrmenezes1@gmail.com>
2024-01-13 13:04:07 -0300

    feat: experimenting with custom token and session objects

--- auth.ts
+++ auth.ts
@@ -14,11 +14,16 @@ export const {
       return true;
     },
     // the session callback uses the token from the jwt callback to get the session and session info
+    // so the session token is identical to the token returned by the jwt callback
+    // and we can customize the session object as well as the token inside the jwt callback to return whatever we want, for instance, add the isNewUser property (e.g.: token.isNewUser = true)
     async session({ session, user, token }) {
+      session.user.customField = token.customField;
       return session;
     },
 
-    async jwt({ token, user, account, profile }) {
+    // user and profile are not as reliable as the token, perhaps they allow for customization but in normal circunstances they may come out as 'undefined', so we'll use the token.sub and pass it as the userId to the session callback to get additional user information
+    async jwt({ token }) {
+      token.customField = "test";
       return token;
     },
   },
