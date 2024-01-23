dca73db5b9aa045b17b0321b3deb0e796cd5284d
pdrmenezes <pdrmenezes1@gmail.com>
2024-01-23 13:23:49 -0300

    chore: bump next-auth to beta5 to fix jwt interface module augmentation not working

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -4,12 +4,11 @@ import "next-auth/jwt";
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string;
       role: "ADMIN" | "USER";
     } & DefaultSession["user"];
   }
 }
-// TODO fix token type declaration
+
 declare module "next-auth/jwt" {
   interface JWT {
     role: "ADMIN" | "USER";
