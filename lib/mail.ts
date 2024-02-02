1be00753348eb2cdd1ff2a692823066d671fd9f1
pdrmenezes <pdrmenezes1@gmail.com>
2024-02-01 21:14:21 -0300

    feat: setup reset password email

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,6 +3,17 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const BASE_URL = "http://localhost:3000";
 
+export async function sendResetPasswordEmail(email: string, token: string) {
+  const resetPasswordLink = `${BASE_URL}/auth/reset-password?token=${token}`;
+
+  await resend.emails.send({
+    // before domain verification it has to be resend's email
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Reset your password",
+    html: `<p><a href='${resetPasswordLink}'>Click here</a> to reset your password.</p>`,
+  });
+}
 export async function sendVerificationEmail(email: string, token: string) {
   const confirmLink = `${BASE_URL}/auth/new-verification?token=${token}`;
 
