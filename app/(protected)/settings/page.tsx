7127f455c2612f273834d94b862cf5b1a143eb2a
pdrmenezes <pdrmenezes1@gmail.com>
2024-02-18 11:11:38 -0300

    feat: handling sign out in client component with client session

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,19 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function SettingsPage() {
   const session = useSession();
+  const onClick = () => {
+    signOut();
+  };
   return (
     <div>
       <h1>Settings Page</h1>
       <p>session info: {JSON.stringify(session)}</p>
-      <form>
-        <button className="p-2 outline" type="submit">
-          Sign Out
-        </button>
-      </form>
+      <button className="p-2 outline" onClick={onClick}>
+        Sign Out
+      </button>
     </div>
   );
 }
