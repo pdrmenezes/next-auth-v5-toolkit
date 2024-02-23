0e11fd83bbe1cef184415948f759b2e5a3f2963c
pdrmenezes <pdrmenezes1@gmail.com>
2024-02-22 23:02:27 -0300

    feat: add server page

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,8 @@
+import { UserInfo } from "@/components/user-info";
+import { currentUser } from "@/lib/auth";
+
+export default async function ServerPage() {
+  const user = await currentUser();
+
+  return <UserInfo label="🗄 Server Component" user={user} />;
+}
