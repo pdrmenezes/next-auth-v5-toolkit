64cace5f6f0b200d37e911d921c17dc484ff3906
pdrmenezes <pdrmenezes1@gmail.com>
2024-02-27 00:57:52 -0300

    feat: improve server action to handle email update

--- actions/update-settings.ts
+++ actions/update-settings.ts
@@ -3,8 +3,10 @@
 import { z } from "zod";
 import { db } from "@/lib/db";
 import { SettingsSchema } from "@/schemas";
-import { getUserById } from "@/data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
 import { currentUser } from "@/lib/auth";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export async function updateSettings(values: z.infer<typeof SettingsSchema>) {
   const user = await currentUser();
@@ -16,6 +18,29 @@ export async function updateSettings(values: z.infer<typeof SettingsSchema>) {
   const userExists = await getUserById(user.id);
   if (!userExists) {
     return { error: "Unauthorized." };
+  }
+
+  // if user is logged in with OAuth provider they can't update these informations
+  if (user.isOAuth) {
+    values.email = undefined;
+    values.password = undefined;
+    values.newPassword = undefined;
+    values.isTwoFactorEnabled = undefined;
+  }
+
+  if (values.email && values.email !== user.email) {
+    const userExists = await getUserByEmail(values.email);
+
+    // if email is in use and it's different from the user who requested the change, return error
+    if (userExists && userExists.id !== user.id) {
+      return { error: "Email already in use" };
+    }
+
+    // if passed the check we'll send a new verification email so the user can verify email ownership
+    const verificationToken = await generateVerificationToken(values.email);
+    await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+    return { success: "Verification email successfully sent." };
   }
 
   await db.user.update({
