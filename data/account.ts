39c11635c7549d77b6f2e7e77fed3033c9d11cd1
pdrmenezes <pdrmenezes1@gmail.com>
2024-02-27 00:17:56 -0300

    feat: helper to get account, if there's an account, user logs in with OAuth

---
+++ data/account.ts
@@ -0,0 +1,10 @@
+import { db } from "@/lib/db";
+
+export async function getAccountByUserId(userId: string) {
+  try {
+    const account = await db.account.findFirst({ where: { userId } });
+    return account;
+  } catch {
+    return null;
+  }
+}
